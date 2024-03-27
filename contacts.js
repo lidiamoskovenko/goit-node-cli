8120826722175bd3f1e66948cf8100b6d974ffa7
lidiamoskovenko <130401685+lidiamoskovenko@users.noreply.github.com>
2024-03-27 05:25:56 +0100

    first

---
+++ contacts.js
@@ -0,0 +1,23 @@
+import { promises as fs } from 'fs';
+
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  // ...твій код. Повертає масив контактів.
+}
+
+async function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
